e2144b1461603f72bbd26b895e561f23fc975c33
unknown <melwinwilson39@gmail.com>
2023-08-19 10:17:31 +0530

    chage

--- main.cpp
+++ main.cpp
@@ -1,57 +1,47 @@
-
-#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <winuser.h>
 
-int getval(){
-    int num{};
-    std::cin >> num;
+using namespace std;
 
-    return num;
-}
+int DisplayResourceNAMessageBox()
+{
+    int msgboxID = MessageBox(
+        NULL,
+        (LPCWSTR)L"Resource not available\nDo you want to try again?",
+        (LPCWSTR)L"Account Details",
+        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+    );
 
-char operant(){
-    char oper;
-    std::cin >> oper;
-
-    return oper;
-}
-
-int cpu(int value,char oper,int value1){
-    if (oper== '+'){
-        return value + value1;
-    }
-    else if (oper == '-') {
-        return value - value1;
-    }
-    else if (oper == '*'){
-        return value * value1;
-    }
-    else if (oper == '/'){
-        return value / value1;
-    }
-    else {
-        return EXIT_FAILURE;
-        exit(1);
+    switch (msgboxID)
+    {
+    case IDCANCEL:
+        // TODO: add code
+        break;
+    case IDTRYAGAIN:
+        // TODO: add code
+        break;
+    case IDCONTINUE:
+        // TODO: add code
+        break;
     }
 
+    return msgboxID;
 }
 
 int main()
 {
-
-    int value {getval()};
-
-    // std::cout << value<<std::endl; //debug
-
-    char oper {operant()};
-
-    /*std::cout << oper;  */           //debug
-
-    int value1 {getval()};
-
-    int  result{cpu(value , oper , value1)};
-
-    std::cout << result<<std::endl;
-
-    return 0;
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    
+    for (const string& word : msg)
+    {
+        cout << word << "\t";
+        dajdhgjdfhgdfjghd
+        
+    }
+    cout << endl;
+    
 }
