37321dfb133ab35775c447b342dc5e7ea4a83f70
unknown <melwinwilson39@gmail.com>
2023-08-22 18:21:39 +0530

    pizzed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,75 @@
-#include  <iostream>
+#include <Windows.h>
 
-int main(){
+// Forward declaration of the window procedure
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Register the window class
+    const wchar_t CLASS_NAME[] = L"SampleWindowClass";
     
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,                          // Optional window styles
+        CLASS_NAME,                 // Window class
+        L"Win32 API Message Box",   // Window title
+        WS_OVERLAPPEDWINDOW,        // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 300,
+
+        NULL,                       // Parent window    
+        NULL,                       // Menu
+        hInstance,                  // Instance handle
+        NULL                        // Additional application data
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    // Display the window
+    ShowWindow(hwnd, nCmdShow);
+
+    // Main message loop
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+// Window procedure callback
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            DrawText(hdc, L"Click the button to show a message box!", -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+
+        case WM_COMMAND:
+            if (LOWORD(wParam) == 1) {  // ID of the button
+                MessageBox(hwnd, L"Hello, this is a message box!", L"Message Box Example", MB_ICONINFORMATION | MB_OK);
+            }
+            return 0;
+    }
+    
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
